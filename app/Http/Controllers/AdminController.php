56fd59a979534c61e216bd2480631e00d77dc498
musfiqur521 <musfiqur521@gmail.com>
2023-11-21 12:10:10 +0600

    database_and_image_valid

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class AdminController extends Controller
 {
     public function post_page()
     {
         return view('admin.post_page');
+    }
+    public function add_post(Request $request)
+    {
+        $post=new Post;
+        $post->title = $request->title;
+        $post->description = $request->description;
+
+        //Image Function Validation
+        $image=$request->image;
+        $imagename=time().'.'.$image->getClientOriginalExtension();
+
+        $request->image->move('postimage',$imagename);
+        $post->image = $imagename;
+
+        $post->save();
+        return redirect()->back();
     }
 }
